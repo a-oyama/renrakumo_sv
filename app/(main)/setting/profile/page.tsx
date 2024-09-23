8f234564f5ef3751f9872afd50e2cc88ff457e3e
a-oyama <snow-sakura@outlook.jp>
2024-09-23 17:15:24 +0900

    プロフィール設定用ページ

---
+++ app/(main)/setting/profile/page.tsx
@@ -0,0 +1,46 @@
+// プロフィール設定ページ
+
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
+import { ProfileType } from "@/types"
+import { Suspense } from "react"
+import Profile from "@/components/settings/Profile"
+import Loading from "@/app/loading"
+
+
+// supabase.auth.getUserでユーザー情報取得
+const ProfilePage = async () => {
+  const supabase = createClient()
+  const { data: userData } = await supabase.auth.getUser()
+  const user = userData?.user
+
+  let profile: ProfileType | null = null
+
+  if (user) {
+    const { data: profileData, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user.id)
+      .single()
+
+    if (error) {
+      console.error("プロフィールの取得中にエラーが発生しました:", error)
+    }
+
+    profile = profileData
+  }
+
+  //プロフィール取得したらProfileコンポーネントへ渡す
+  if (!user || !profile) {
+    redirect("/")
+  }
+
+  // Suspenseでローディング画面表紙
+  return (
+    <Suspense fallback={<Loading />}>
+      <Profile profile={profile} />
+    </Suspense>
+  )
+}
+
+export default ProfilePage
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+
+// プロフィール型
+export interface ProfileType {
+    id: string
+    name: string
+    introduce: string | null
+    avatar_url: string | null
+  }
