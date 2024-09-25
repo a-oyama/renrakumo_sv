eca195c747870d4ae1aa764479e09f732469211c
a-oyama <snow-sakura@outlook.jp>
2024-09-25 23:08:30 +0900

    カレンダーの日表示を消す

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 // メインページ
-import React from "react"
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from '@fullcalendar/daygrid'
+//カレンダーの日付を消す
+import { DayCellContentArg } from "@fullcalendar/core/index.js"
 
 const MainPage = () => {
   return (
@@ -13,6 +14,9 @@ const MainPage = () => {
         initialView="dayGridMonth"
         height="auto"
         locale="jp"
+        dayCellContent={(event: DayCellContentArg) =>
+          (event.dayNumberText = event.dayNumberText.replace("日", ""))
+        }
          />
       </div>
     </div>
