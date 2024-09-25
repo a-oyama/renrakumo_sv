6339650b33fe22cd0c5ed08acb905f39626e66a0
a-oyama <snow-sakura@outlook.jp>
2024-09-25 22:01:14 +0900

    fullcalendar表示

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,18 @@
 // メインページ
+import React from 'react'
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 const MainPage = () => {
     return (
       <div>
         <div>メインページ</div>
+        <div>
+          
+        <FullCalendar
+         plugins={[dayGridPlugin]}
+         initialView="dayGridMonth" />
+        </div>
       </div>
     )
   }
