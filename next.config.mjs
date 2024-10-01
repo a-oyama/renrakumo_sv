b5d88b173833c52fad3a2a7bdce824ac19077d58
a-oyama <snow-sakura@outlook.jp>
2024-10-01 21:34:03 +0900

    プロフ画像登録するとエラーになる
    remotePatterns内に設定を記述

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 //supabaseからの画像取得許可
+//httpsとURLは分割しないとダメ！！！
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
           {
-            protocol: "https",
-            hostname: "https://vyejpflxsreajoxeryck.supabase.co",
+            protocol: 'https',
+//            hostname: 'https://vyejpflxsreajoxeryck.supabase.co',
+            hostname: 'vyejpflxsreajoxeryck.supabase.co',
+
+            port: '',
           },
         ],
       },
