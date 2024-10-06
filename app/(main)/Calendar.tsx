3b7bccd54ce76eecdcbdc1de545cf03490604f0a
a-oyama <snow-sakura@outlook.jp>
2024-10-06 10:55:53 +0900

    カレンダーへの予定登録
    supabaseに登録されているので画面再描画で対処

--- app/(main)/Calendar.tsx
+++ app/(main)/Calendar.tsx
@@ -1,34 +1,24 @@
-
-import { useCallback, useState, useEffect, useRef } from "react"
+import { useState, useEffect } from "react"
 import { createClient } from "@/utils/supabase/client"
-
-import FullCalendar, { CalendarApi } from '@fullcalendar/react'
-// 月カレンダー
+import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-// 日を消す
 import { DayCellContentArg } from "@fullcalendar/core/index.js"
-// 日本語カレンダー
 import allLocales from '@fullcalendar/core/locales-all'
-// カレンダーをクリックできるようにする
-// スマホは長押し(selectLongPressDelay)
 import interactionPlugin from "@fullcalendar/interaction"
 import { DateSelectArg } from "@fullcalendar/core/index.js"
 import { EventClickArg } from "@fullcalendar/core/index.js"
 
-
-
+import { useRouter } from "next/navigation"
 
 const Calendar = () => {
   const supabase = createClient()
-  // 状態の確認
+  const router = useRouter()
   const [events, setEvents] = useState<EventInit[]>([]);
   
-  // 初期化
 useEffect(() => {
   fetchEvents();
 }, []);
 
-  // Supabase からデータを取得
   const fetchEvents = async () => {
     const { data, error } = await supabase
     .from('events')
@@ -39,7 +29,6 @@ useEffect(() => {
        else setEvents(data);
 };
 
-  // イベント登録
   const handleDateSelect = async (selectInfo: DateSelectArg) => {
     
     if (!selectInfo) {
@@ -57,43 +46,23 @@ useEffect(() => {
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
-//        allDay: true,
       };
 
       const { data, error } = await supabase
       .from('events')
       .insert([newEvent]);
 
-/*       if (error) {
-        console.error('データの挿入に失敗しました:', error)
-      } else {
 
-        calendarApi.addEvent({
-          id: data[0].id,  // Supabase で生成されたIDを使用
-          ...newEvent,
-        }); */
-
-        // if文でdataがnullでないことを確認
-         if (error || !data || data.length === 0) { // data の存在を確認
+         if (error || !data || data.length === 0) { // if文でundefind防止
           console.error('データの挿入に失敗しました:', error);
         } else {
-          calendarApi.addEvent({
-            id: data[0].id,
-            ...newEvent,
-          });
-/*           if (error || !data || data.length === 0) {
-            console.error('データの挿入に失敗しました:', error);
-          } else {
-            // `events` ステートを更新して、新しいイベントを追加する
-            setEvents((prevEvents) => [
-              ...prevEvents,
-              { id: data[0].id, ...newEvent },
-            ]); */
+          fetchEvents();
       }
+
+          window.location.reload(); //処理終了時にリロード
     }
   };
 
-  // イベント削除
   const handleEventClick = async (clickInfo: EventClickArg) => {
     if (window.confirm(`'${clickInfo.event.title}' を削除しますか？`)) {
       const { error } = await supabase
@@ -124,12 +93,12 @@ useEffect(() => {
       select={handleDateSelect}
       eventClick={handleEventClick}
       editable={true}
-      displayEventTime={false}  // 時間の表示を無効化し終日とする
+      displayEventTime={false}
       dayMaxEvents={true}
+      businessHours={true}
     />
   );
 
 }
-
 
 export default Calendar;
